a7dc5a2783a77736934d1f5e6825bf695bf9e445
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-20 15:28:54 +0100

    add: js variables + event click + form values

--- esercizio-js/esercizio.js
+++ esercizio-js/esercizio.js
@@ -1,30 +1,44 @@
-let etaUtente = 18;
-let kmUtente = 10;
+// let etaUtente = 18;
+// let kmUtente = 10;
 
-console.log(`la tua età: ${etaUtente}`)
-console.log(`i km che percorrerai: ${kmUtente}`)
+// console.log(`la tua età: ${etaUtente}`)
+// console.log(`i km che percorrerai: ${kmUtente}`)
 
-//calcolo costo iniziale biglietto
+// //calcolo costo iniziale biglietto
 
-const prezzoPerKm = 0.21;
-let costoBiglietto = kmUtente * 0.21;
+// const prezzoPerKm = 0.21;
+// let costoBiglietto = kmUtente * 0.21;
 
-console.log(`costo iniziale biglietto: ${costoBiglietto}`)
+// console.log(`costo iniziale biglietto: ${costoBiglietto}`)
 
-//verifica sconti
+// //verifica sconti
 
-if (etaUtente < 18) {
-    console.log(`sconto per minorenni del 20% applicato`);
-    costoBiglietto = costoBiglietto - [(costoBiglietto * 20) / 100];
-} else if (etaUtente > 65) {
-    console.log(`sconto per over 65 del 40% applicato`);
-    costoBiglietto = costoBiglietto - [(costoBiglietto * 40) / 100];
-}
+// if (etaUtente < 18) {
+//     console.log(`sconto per minorenni del 20% applicato`);
+//     costoBiglietto = costoBiglietto - [(costoBiglietto * 20) / 100];
+// } else if (etaUtente > 65) {
+//     console.log(`sconto per over 65 del 40% applicato`);
+//     costoBiglietto = costoBiglietto - [(costoBiglietto * 40) / 100];
+// }
 
-//massimo due decimali
+// //massimo due decimali
 
-costoBiglietto = costoBiglietto.toFixed(2);
+// costoBiglietto = costoBiglietto.toFixed(2);
 
-//risultato
+// //risultato
 
-console.log(`il costo finale del tuo biglietto è: ${costoBiglietto} `)
+// console.log(`il costo finale del tuo biglietto è: ${costoBiglietto} `)
+
+let nameSurname = "";
+let km = 0;
+let age = "";
+let button = document.getElementById("btnTicket");
+
+button.addEventListener("click", () => {
+    event.preventDefault();
+
+    nameSurname = document.getElementById("nameSurname").value;
+    km = document.getElementById("km").value;
+    age = document.getElementById("ages").value;
+    
+})
